4575bcaf9404e7a3a31ab99ff8dd97371de84e2c
Tiago Ribeiro <tiago.ribeiro0794@gmail.com>
2024-03-13 17:49:38 +0000

    #11 Add S3 bucket resource

--- main.tf
+++ main.tf
@@ -4,16 +4,26 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.0"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.40.0"
+    }
   }
 }
 
-provider "random" {
-  # Configuration options
-}
+provider "random" { }
+provider "aws" { }
 
 resource "random_string" "bucket_name" {
     length           = 16
+    lower            = true
+    upper            = false
     special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
 }
 
 output "random_output_result" {

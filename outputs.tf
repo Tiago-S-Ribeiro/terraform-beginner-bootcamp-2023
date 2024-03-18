b85657e090b6552dbff94bb2a4f9c8273a2a0aa4
Tiago Ribeiro <tiago.ribeiro0794@gmail.com>
2024-03-18 20:14:18 +0000

    #34 Invalidate cloudfront cache when content_version is incremented

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "website_endpoint" {
     description = "S3 static website hosting endpoint"
     value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+    description = "The cloudfront distribution domain name"
+    value = module.terrahouse_aws.cloudfront_url
+}
